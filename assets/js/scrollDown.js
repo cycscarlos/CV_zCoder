3e75bf7b00513ecf8c746a045a5308a73803798c
zCoder <cycs.ingenieria@gmail.com>
2023-11-29 16:26:35 -0400

    Se eliminó el botón de scroll down y se incluyó el de scroll up

--- assets/js/scrollDown.js
+++
@@ -1,9 +0,0 @@
-//: SHOW SCROLL Down  */
-function scrollDown() {
-  const scrollDown = document.getElementById("scroll-down");
-  // When the scroll is higher than 50 and less 800 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if (this.scrollY >= 50 && this.scrollY <= 800)
-    scrollDown.classList.add("show-scrollDown");
-  else scrollDown.classList.remove("show-scrollDown");
-}
-window.addEventListener("scroll", scrollDown);
